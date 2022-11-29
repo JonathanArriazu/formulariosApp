cea7b6f10af5dac99d883ad6a95b188df517c66e
JoniArriazu <joni.arriazu2@gmail.com>
2022-11-29 18:22:17 -0300

    the form was created and some validations were added

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -6,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicosComponent implements OnInit {
 
+  @ViewChild('miFormulario') miFormulario !: NgForm;
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  /* guardar( miFormulario: NgForm) {
+    console.log(miFormulario);
+  } */
+
+  nombreValido(): boolean {
+    return this.miFormulario?.controls['producto']?.invalid && this.miFormulario?.controls['producto']?.touched
+    //Si no colocamos el miFormulario?., lo que pasa es que nos muestra un error de que no puede leer la propiedad "controls" de undefined, esto sucede porque el formulario se ejecuta al principio como undefined hasta que yo ingreso a el. Entonces, al colocar .? le digo que si tenemos miFormulario, entonces que siga evaluando con .controls, sino, no.
+  }
+
+  precioValido(): boolean {
+    return this.miFormulario?.controls['precio']?.value<=0 && this.miFormulario?.controls['producto']?.touched
+    //Muestro el mensaje cuando el precio sea menor a 0, por eso es que escribo value<=0
+  }
+
+
+
+  guardar() {
+    console.log(this.miFormulario);
   }
 
 }
