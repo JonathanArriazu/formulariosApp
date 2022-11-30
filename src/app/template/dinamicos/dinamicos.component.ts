4a1b33a7a88b3aab8dc9867f38d7711ebb9dad3f
JoniArriazu <joni.arriazu2@gmail.com>
2022-11-30 01:30:07 -0300

    Add and Delete item was added to Dynamic form page

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,12 +1,38 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
+
+interface Favorito {
+  id: number;
+  nombre: string;
+}
+
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html'
 })
 export class DinamicosComponent implements OnInit {
 
+  nuevoJuego: string = '';
+
+  persona: Persona = {
+    nombre: 'Jonathan',
+    favoritos: [
+      {
+        id: 1,
+        nombre: 'Metal Gear'
+      },
+      {
+        id: 2,
+        nombre: 'DeathStrranding'
+      }
+    ]
+  }
+  
   @ViewChild('miFormulario') miFormulario !: NgForm;
 
   constructor() { }
@@ -20,6 +46,21 @@ export class DinamicosComponent implements OnInit {
   
   guardar() {
     console.log('Hola');
+  }
+
+  agregarJuego() {
+    const nuevoFavorito : Favorito = {
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoJuego
+    }
+
+    this.persona.favoritos.push({...nuevoFavorito});
+    this.nuevoJuego = '';
+
+  }
+
+  eliminar( index:number ) {
+    this.persona.favoritos.splice(index, 1) //Posicion desde la que quiero borrar y cuantos elementos quiero borrar (seria el mismo elemento si coloco un 1)
   }
 
 }
