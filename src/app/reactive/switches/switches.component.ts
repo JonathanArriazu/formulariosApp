18315a9c62a8ba909f488eebd472cf9931c1ed12
JoniArriazu <joni.arriazu2@gmail.com>
2022-11-30 17:21:24 -0300

    Functionalities and validations were added to switches page

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,14 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html'
 })
-export class SwitchesComponent implements OnInit {
+export class SwitchesComponent implements OnInit{
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    genero: ['M', Validators.required],
+    notificaciones: [true, Validators.required],
+    terminos: [false, Validators.requiredTrue]
+  });
 
-  ngOnInit(): void {
+  persona = {
+    genero: 'F',
+    notificaciones: true,
+  }
+
+  constructor( private fb: FormBuilder ) { }
+
+  ngOnInit() {
+    
+    this.miFormulario.reset( {
+      ...this.persona,
+      terminos: false
+    } );
+    //usamos reset, ya que en miFormulario tenemos terminos y en persona no, entonces
+    //si usamos setValue en lugar de reset, marcaria error.
+
+    this.miFormulario.valueChanges.subscribe( ({condiciones, ...rest}) => {
+      /* delete form.condiciones; */
+      this.persona = rest;
+    })
+  }
+
+  guardar() {
+    const formValue = {...this.miFormulario.value}
+    delete formValue.condiciones;
+    
+    this.persona = formValue;
   }
 
 }
