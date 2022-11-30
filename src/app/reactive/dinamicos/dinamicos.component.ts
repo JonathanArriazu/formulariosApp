9c12a9bc81904b8ed9f2dbcd80e0c954091700fe
JoniArriazu <joni.arriazu2@gmail.com>
2022-11-30 16:51:24 -0300

    Add, Delete functions and validations were added to dynamic page

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { FormGroup, Validators, FormBuilder, FormArray, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -8,8 +8,20 @@ import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 export class DinamicosComponent implements OnInit {
 
   miFormulario: FormGroup = this.fb.group({
-    nombre: ['', [Validators.required, Validators.minLength(3)]]
+    nombre: ['', [Validators.required, Validators.minLength(3)]],
+    favoritos: this.fb.array([//Indico otro fb.array para inicializar 
+    //un array, ya que si lo coloco como [] con esto estaria directamente aplicando un nuevo campo
+    //El colocar [] es igual a que coloque this.fb.control()
+    ['Metal Gear'],
+    ['Death Stranding']
+    ], Validators.required)
   })
+
+  nuevoFavorito: FormControl = this.fb.control('', Validators.required);
+
+  get favoritosArr() {
+    return this.miFormulario.get('favoritos') as FormArray;
+  }
 
   constructor( private fb: FormBuilder) { }
 
@@ -21,6 +33,17 @@ export class DinamicosComponent implements OnInit {
             && this.miFormulario.controls?.[campo]?.touched
   }
 
+  agregarFavorito() {
+    if (this.nuevoFavorito.invalid) {
+      return;
+    }
+
+    this.favoritosArr.push( this.fb.control( this.nuevoFavorito.value, Validators.required ) );
+
+    this.nuevoFavorito.reset();
+
+  }
+
   guardar() {
     if (this.miFormulario.invalid) {
       this.miFormulario.markAllAsTouched();
@@ -29,6 +52,10 @@ export class DinamicosComponent implements OnInit {
 
     console.log(this.miFormulario.value);
     this.miFormulario.reset();
+  }
+
+  eliminar(index: number) {
+    this.favoritosArr.removeAt(index);
   }
 
 }
