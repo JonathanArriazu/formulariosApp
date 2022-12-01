eb0ed4b0869373e4221517ed3fb37e1068ebbe27
JoniArriazu <joni.arriazu2@gmail.com>
2022-12-01 16:17:16 -0300

    Login and register page were created and name validation was implemented to register page

---
+++ src/app/auth/pages/registro/registro.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.component.html'
+})
+export class RegistroComponent implements OnInit {
+
+  nombreApellidoPattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+
+  miFormulario: FormGroup = this.fb.group({
+    nombre:['', [Validators.required, Validators.pattern(this.nombreApellidoPattern)]]
+  })
+
+  constructor( private fb: FormBuilder ) { }
+
+  ngOnInit(): void {
+  }
+
+  campoNoValido(campo: string) {
+    return this.miFormulario.get(campo)?.invalid
+            && this.miFormulario.get(campo)?.touched
+  }
+
+  submitFormulario() {
+
+    console.log(this.miFormulario.value);
+
+    this.miFormulario.markAllAsTouched();
+
+  }
+
+}
